714e6ae15297d0f76b09273263668da42ac287f0
Bhavik Bhatt <bhattbhavikr340@gmail.com>
2023-02-22 23:38:54 +0530

    Initial Commit

---
+++ NasaGallery/NasaGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaGalleryApp.swift
+//  NasaGallery
+//
+//  Created by Bhavik Bhatt on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NasaGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
